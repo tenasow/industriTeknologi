2831a08d6647e260d304c2a883f3106b5202502f
tenasow <bjarke.mouritzen@gmail.com>
2017-05-03 10:21:19 +0200

    added stuff?

--- index.php
+++ index.php
@@ -4,10 +4,43 @@
   </head>
   <body>
     <div class="container">
-      <h1>Test</h1>
-      <?php
-        echo "Hej\n";
-      ?>
+      <div class="content">
+        lys: <span id="lys">
+
+        <br>
+
+        <br>
+
+        temp: <span id="temp">
+
+        <br><br>
+
+        Co2: <span id="co2">
+
+        <br><br>
+
+        lyd: <span id="lyd">
+
+      </span></span></span></span></div>
+
+      <br>
+
+      <form action="index.php">
+
+        <input type="textbox" name="temp" placeholder="set temp">
+        <br>
+        <input type="textbox" name="lys" placeholder="set lys">
+        <br>
+        <input type="textbox" name="co2" placeholder="set Co2">
+        <br>
+        <input type="textbox" name="lyd" placeholder="set lyd">
+        <br>
+        <input type="submit" value="submit">
+      </form>
+
+      <div class="footer">
+        Made by Emil, Mathias og Bjarke - Industriteknologi 03-05-2017
+      </div>
     </div>
   </body>
 </html>
