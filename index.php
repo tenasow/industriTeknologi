1dde574aee1ef727cfcc7a1a53a3bba4a2b14f50
Emil Olesen <emil8250@ymail.com>
2017-05-03 10:01:44 +0200

    Added bootstrap

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Test</h1>
+      <?php
+        echo "Hej\n";
+      ?>
+    </div>
+  </body>
+</html>
